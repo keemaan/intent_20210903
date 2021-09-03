3002519d9d3fd371d412cb776856bf33b44dcd5a
keemaan <keemaan@naver.com>
2021-09-03 15:28:37 +0900

    intent(3) edit Nickname reback, main reflish X

--- app/src/main/java/com/example/intent_20210903/EditNicknameActivity.kt
+++ app/src/main/java/com/example/intent_20210903/EditNicknameActivity.kt
@@ -1,11 +1,23 @@
 package com.example.intent_20210903
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+        okBtn.setOnClickListener {
+            val inputNickname = nicknameEdt.text.toString()
+            val resultIntent = Intent()
+            resultIntent.putExtra("newNickname",inputNickname)
+
+//      if okBtn click
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
     }
 }
