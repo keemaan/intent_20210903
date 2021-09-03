0359da98b321dbe4c04299c7d3a0fc067bbd2efa
keemaan <keemaan@naver.com>
2021-09-03 14:26:13 +0900

    finish()

--- app/src/main/java/com/example/intent_20210903/OtherActivity.kt
+++ app/src/main/java/com/example/intent_20210903/OtherActivity.kt
@@ -2,10 +2,17 @@ package com.example.intent_20210903
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        returnToMainBtn.setOnClickListener {
+            finish()
+
+        }
+
     }
 }
