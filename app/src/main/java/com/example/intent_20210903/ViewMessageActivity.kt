ff8114c0247de15e47364bb4580eed7b99fb0301
keemaan <keemaan@naver.com>
2021-09-03 14:46:05 +0900

    parameta data UI complete(string)

--- app/src/main/java/com/example/intent_20210903/ViewMessageActivity.kt
+++ app/src/main/java/com/example/intent_20210903/ViewMessageActivity.kt
@@ -2,10 +2,15 @@ package com.example.intent_20210903
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_message.*
 
 class ViewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_message)
+
+        val receivedMessage = intent.getStringExtra("inputMessage")
+
+        messageTxt.text = receivedMessage
     }
 }
