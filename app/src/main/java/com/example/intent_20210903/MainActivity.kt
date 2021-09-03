7e91fe393dfdf3b9eb225350fd8cbe5599b00a47
keemaan <keemaan@naver.com>
2021-09-03 14:21:53 +0900

    main->other activity

--- app/src/main/java/com/example/intent_20210903/MainActivity.kt
+++ app/src/main/java/com/example/intent_20210903/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.intent_20210903
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherBtn.setOnClickListener {
+
+        val myIntent = Intent(this,OtherActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
